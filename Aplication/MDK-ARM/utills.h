57eb1ad11e011a88d1fd9e7c9d37b54c49888f2d
MauroTrendX <mauro.ribeiro@trendxbrasil.com>
2021-07-20 16:49:51 -0300

    adjusted Print in LOG

--- Aplication/MDK-ARM/utills.h
+++ Aplication/MDK-ARM/utills.h
@@ -7,13 +7,13 @@
 
 //#define PRECISION 1; /* define this only if you're going
 //#define SHOWMEASURES/*use this to print the mean of adc acquisition in ADC.h*/
-//#define SHOWADC_SAMPLE /* define this if you want to see the adc value without the mean in ADC.h*/
+#define SHOWADC_SAMPLE /* define this if you want to see the adc value without the mean in ADC.h*/
 //#define MAUROTESTE/* use this to enable slot of test 1 in main.c*/
 #define MAUROTESTE_2/* define this to enable slot of test 2* in main.c*/
 //#define ADV_ALL/* define this if you want to use all available data on function HR_advdata_manuf_data_update inside main.c*/
 //#define CAL_SET_SHOW /*define this to print all the output status of  ADS018_Cal_Set in main.c*/
 //#define TESTADV /* define this if you want to test  stop and restart of the advertising functionality in main.c*/
-#define SHOWSTATS//define this if you wanna show load power and rpm in heritage.h
+//#define SHOWSTATS//define this if you wanna show load power and rpm in heritage.h
 //#define SHOWFILTER_OUT4 /*define this if you want to see the parameter out4 of function filter in heritage.h*/
 //#define SHOWDIRECTION /*define this if you want to see the direction outuput of function get_direction in heritage.h*/
 
